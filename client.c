cd1c29a5f2a13749d487c7847ac07cdd5eacff39
Gabriel Gimenez <12-11006@usb.ve>
2016-02-13 14:52:09 -0430

    Client ready.

--- client.c
+++ client.c
@@ -13,32 +13,36 @@
 #define ALTO 4
 
 WINDOW *ventanaOutput, *ventanaInput;
+int name_dec, server_dec;
+char name[50], to_server[55];
 
-int readLine(int fd, char *str) {
-        int n;
-        do {
-                n = read(fd, str, 1);
-        } while(n > 0 && *(str++) != '\0');
-return(n > 0);
+void close_client(){
+	unlink(name);
+	unlink(to_server);
+	endwin();
 }
 
 void send_hello(char * name, char * pipe){
-	int fd, messagelen,i;
-	char name_serv[55];
+	int fd, messagelen;
  	char message[150];
 	unlink(name);
         mknod(name, S_IFIFO, 0);
         chmod(name, 0660);
-        strcpy(name_serv, name);
-        strcat(name_serv, "_serv");
-        unlink(name_serv);
-        mknod(name_serv, S_IFIFO, 0);
-        chmod(name_serv, 0660);
+	sprintf(to_server, "%s_serv", name);
+        unlink(to_server);
+        mknod(to_server, S_IFIFO, 0);
+        chmod(to_server, 0660);
 	sprintf(message, "New user: %s", name);
- 	fd = open(pipe, O_WRONLY);
+	fd = open(pipe, O_WRONLY | O_NONBLOCK);
+ 	if ( fd == -1) {
+		close_client();
+		printf("No se ha podido conectar al servidor [%s]\n", pipe);
+		exit(1);
+	};
 	messagelen = strlen(message) + 1; 
+	name_dec = open(name, O_RDONLY | O_NONBLOCK);
    	write(fd, message, messagelen);
- 	close(fd);
+	do { server_dec = open(to_server, O_WRONLY | O_NONBLOCK); } while (server_dec == -1);
 }
 
 void limpiarVentanaInput(){
@@ -56,24 +60,30 @@ void enfocarInput(){
 }
 
 void term_handler(){
-	//printf("It worked");
-	endwin();
+	char tmp[50];
+	sprintf(tmp, "%s -exit", name);
+	write(server_dec, tmp, strlen(tmp)+1);
+	close_client();
 	exit(0);
 }
 
+
 void main(int argc, char * argv[]){
-	int file, max_desc, desc[2];
-	char name[50], to_server[50], message[306], command[256],pipe[100];
-	char probando[200], c[2];
+	// Declaraciones
+	int max_desc, desc[2];
+	char message[306], command[256],pipe[100];
+	char buffer[200] = "", c[2] = {0,'\0'};
 	fd_set lectura;
 	fd_set c_lectura;
 	struct timeval tv;
-	int retval,status,i,alto,x,y,salir = 0;
+	int status,i,alto,x,y,salir = 0;
+	
+	// Inicializar senales.
 	if (signal(SIGINT, term_handler) == SIG_ERR) {
         	printf("An error occurred while setting a signal handler.\n");
     	}
 
-	// Interfaz
+	// Interfaz.
 	initscr();
 	alto = LINES - ALTO;
 	ventanaOutput = newwin(alto, 0, 0, 0);
@@ -88,7 +98,10 @@ void main(int argc, char * argv[]){
 	limpiarVentanaInput();
 	// Fin Interfaz
 
+	// Obtener usuario.
 	strcpy(name,getlogin());
+
+	// Obtener pipe.
 	strcpy(pipe, DEFAULT_PIPE);
 	if (argc > 1) {
 		for (i = 1; i < argc; i ++){
@@ -100,99 +113,76 @@ void main(int argc, char * argv[]){
 			}
 		}
 	}
+
+	// Identificarse con el servidor.
 	send_hello(name, pipe);
-	strcpy(to_server, name);
-	strcat(to_server, "_serv");
-	do {
-		desc[0] = open(name, O_RDONLY | O_NONBLOCK);
-	} while (desc[0] == -1);
-	//desc[1] = 0;	
+	sprintf(to_server, "%s_serv", name);
+	wprintw(ventanaOutput, "Conectando al servidor. [%s]\n", pipe);
+	wrefresh(ventanaOutput);
+	enfocarInput();
+	
+	// Inicializando select
+	desc[0] = name_dec;
 	FD_ZERO(&lectura);
-	FD_SET(desc[1], &lectura);
 	FD_SET(desc[0], &lectura);
 	max_desc = desc[0]+1;
-	//printf("Connecting to the server...\n");
-	c[1] = '\0';
-	memset(probando,0,200);
+
+	// Inicializando manejador de input.
 	while(!salir){
-		//wprintw(ventanaOutput, "mensaje");
-	//	wrefresh(ventanaOutput);
 		tv.tv_sec = 0;
 		tv.tv_usec = 10000;
 		c_lectura = lectura;
 		if (select(max_desc, &c_lectura, NULL, NULL, &tv) < 0)
-			perror("SELECT FAILED");
+			perror("Error en el select");
 		for (i = 0; i < 1; i++){
 			if( FD_ISSET(desc[i], &c_lectura)){
-				memset(message, 0, sizeof(message));
 				status = read(desc[i], message, sizeof(message));
-				//status = readLine(desc[i],message);
 				if (status){	
-					if ( i == 1) {
-						//Envio mensaje al servidor
-						//printf("Mensaje enviado al servidor: %s", message);
-					//	if ( message[0] == '\n' ) {
-					//		sprintf(command, "%s %s", name, message);
-					//		file = open(to_server, O_WRONLY | O_NDELAY);
-					//		if ( file == -1 )
-					//			perror("error");
-					//		write(file, command, strlen(command)+1);
-					//		wprintw(ventanaOutput, "%s", command);
-					//		wrefresh(ventanaOutput);
-					//		enfocarInput();
-							salir = strcmp(message,"-exit\n") == 0;
-					//		limpiarVentanaInput();
-					//	} else {
-					//		enfocarInput();
-					//	}
-					} else {
-						// Es un mensaje del servidor
-						//printf("%s", message);
-						//fflush(stdout);
-						wprintw(ventanaOutput, "%s", message);
-						wrefresh(ventanaOutput);
-						enfocarInput();
-					}
+					wprintw(ventanaOutput, "%s", message);
+					wrefresh(ventanaOutput);
+					enfocarInput();
 				} else {
-					//printf("NO STATUS\n");
+					close_client();
+					printf("El sevidor se ha cerrado.\n");
+					exit(1);
 				}
-				}	
-			}
+				memset(message, 0, sizeof(message));
+			}	
+		}
 
-					
+		//  Manejador de input por teclado.			
 		c[0] = wgetch(ventanaInput);
                 if (c[0] == '\r'){
-			sprintf(command, "%s %s\n", name, probando);
-			file = open(to_server, O_WRONLY | O_NDELAY);
-			if ( file == -1 )
-				perror("error");
-			write(file, command, strlen(command)+1);
-                        wprintw(ventanaOutput, "<%s> %s\n", name, probando);
+			// Es un mensaje al servidor.
+			sprintf(command, "%s %s\n", name, buffer);
+			write(server_dec, command, strlen(command)+1);
+                        wprintw(ventanaOutput, "<%s> %s\n", name, buffer);
                         wrefresh(ventanaOutput);
-                        memset(probando,0,200);
+                        memset(buffer,0,200);
                         limpiarVentanaInput();
 			enfocarInput();
                 } else if (c[0] == 127) {
+			// Se quiere borrar un caracter.
 			getyx(ventanaInput,y,x);
 			wmove(ventanaInput,y,--x);
-			probando[(y-1)*COLS+x] = '\0';
+			buffer[(y-1)*COLS+x] = '\0';
 			wdelch(ventanaInput);
 		} else if (c[0] == '\033'){
+			// Deshabilita las flechas del teclado.
 			wgetch(ventanaInput);
 			wgetch(ventanaInput);
 		} else {			
 			if (c[0] != ERR){
-			getyx(ventanaInput,y,x);
-			wprintw(ventanaInput,"%c", c[0]);
-			wmove(ventanaInput,y,++x);
-			wrefresh(ventanaInput);
-                        strcat(probando,c);
+			// Formar mensaje.
+				getyx(ventanaInput,y,x);
+				wprintw(ventanaInput,"%c", c[0]);
+				wmove(ventanaInput,y,++x);
+				wrefresh(ventanaInput);
+				strcat(buffer,c);
 			} 
                 }
 	}
-	endwin();
-	unlink(name);
-	unlink(to_server);
-	exit(0);
 
+	close_client();
+	exit(0);
 }
